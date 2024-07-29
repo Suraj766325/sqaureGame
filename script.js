12053de25d623486e7bb9411ed17a01b5b8423fe
Suraj Kumar <155295926+Suraj766325@users.noreply.github.com>
2024-07-29 12:39:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let a=document.getElementById("b1");
+let b=document.getElementById("b2");
+let c=document.getElementById("b3");
+let d=document.getElementById("b4");
+
+
+a.addEventListener("mouseenter",function(){
+    a.innerHTML=`<h1>${Math.ceil(100*Math.random())}</h1>`;
+})
+a.addEventListener("mouseleave",function(){
+    a.innerHTML="<h1>4</h1>";
+})
+
+let clr="red";
+b.addEventListener("mouseenter",function(){
+    if(clr=="red"){
+        b.style.backgroundColor="green";
+        clr="green";
+    }
+    else{
+        b.style.backgroundColor="red";
+        clr="red";
+    }
+})
+b.addEventListener("mouseleave",function(){
+    b.style.backgroundColor="white";
+})
+
+
+
+// rgb(a,b,c) where a,b,c lies between 0 10 250
+c.addEventListener("mouseenter",function(){
+    let r1=Math.floor(256*Math.random());
+    let r2=Math.floor(256*Math.random());
+    let r3=Math.floor(256*Math.random());
+    c.style.backgroundColor=`rgb(${r1},${r2},${r3})`;
+})
+c.addEventListener("mouseleave",function(){
+    c.style.backgroundColor="white";
+})
+
+d.addEventListener("click",function(){
+    let r4=Math.floor(256*Math.random());
+    let r5=Math.floor(256*Math.random());
+    let r6=Math.floor(256*Math.random());
+    a.style.backgroundColor=`rgb(${r4},${r5},${r6})`;
+    b.style.backgroundColor=`rgb(${r6},${r4},${r5})`;
+    c.style.backgroundColor=`rgb(${r5},${r6},${r4})`;
+})
+d.addEventListener("mouseleave",function(){
+    a.style.backgroundColor="white";
+    b.style.backgroundColor="white";
+    c.style.backgroundColor="white";
+})
+
+
+
+
